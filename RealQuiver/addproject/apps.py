69b3140435d3c4843b533dc178aee137f8e2a75f
acornph09 <acornph09@yahoo.com.ph>
2017-03-06 22:21:31 +0800

    Removed unnecessary directory

--- RealQuiver/addproject/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AddprojectConfig(AppConfig):
-    name = 'addproject'
--- RealQuiver/addproject/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.core.urlresolvers import reverse
-
-class File(models.Model):
-    file_name = models.FileField(upload_to='media/')
-# Create your models here.
